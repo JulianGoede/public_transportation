aed92da956f310e000f7c2937bfb02820ca8625f
Julian Goede <julian.goede@smartclip.tv>
2020-10-23 16:43:35 +0200

    implemented weather producer using rest-api and defined value schema

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -4,12 +4,10 @@ import json
 import logging
 from pathlib import Path
 import random
-import urllib.parse
 
 import requests
 
 from models.producer import Producer
-
 
 logger = logging.getLogger(__name__)
 
@@ -30,16 +28,11 @@ class Weather(Producer):
     summer_months = set((6, 7, 8))
 
     def __init__(self, month):
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
         super().__init__(
-            "weather", # TODO: Come up with a better topic name
+            "com.udacity.producers.models.weather.v2",
             key_schema=Weather.key_schema,
             value_schema=Weather.value_schema,
+            num_partitions=2
         )
 
         self.status = Weather.status.sunny
@@ -51,14 +44,11 @@ class Weather(Producer):
 
         if Weather.key_schema is None:
             with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
-                Weather.key_schema = json.load(f)
+                Weather.key_schema = json.dumps(json.load(f))
 
-        #
-        # TODO: Define this value schema in `schemas/weather_value.json
-        #
         if Weather.value_schema is None:
             with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
-                Weather.value_schema = json.load(f)
+                Weather.value_schema = json.dumps(json.load(f))
 
     def _set_weather(self, month):
         """Returns the current weather"""
@@ -72,38 +62,23 @@ class Weather(Producer):
 
     def run(self, month):
         self._set_weather(month)
-
-        #
-        #
-        # TODO: Complete the function by posting a weather event to REST Proxy. Make sure to
-        # specify the Avro schemas and verify that you are using the correct Content-Type header.
-        #
-        #
-        logger.info("weather kafka proxy integration incomplete - skipping")
-        #resp = requests.post(
-        #    #
-        #    #
-        #    # TODO: What URL should be POSTed to?
-        #    #
-        #    #
-        #    f"{Weather.rest_proxy_url}/TODO",
-        #    #
-        #    #
-        #    # TODO: What Headers need to bet set?
-        #    #
-        #    #
-        #    headers={"Content-Type": "TODO"},
-        #    data=json.dumps(
-        #        {
-        #            #
-        #            #
-        #            # TODO: Provide key schema, value schema, and records
-        #            #
-        #            #
-        #        }
-        #    ),
-        #)
-        #resp.raise_for_status()
+        resp = requests.post(
+            f"{Weather.rest_proxy_url}/topics/{self.topic_name}",
+            headers={"Content-Type": "application/vnd.kafka.avro.v2+json"},
+            data=json.dumps(
+                {
+                    # schemas must be dumped to string when providing avro schema
+                    "key_schema": Weather.key_schema,
+                    "value_schema": Weather.value_schema,
+                    "records": [
+                        {"key": {'timestamp': self.time_millis()},
+                         "value": {'temperature': self.temp, 'status': self.status.name}
+                         }
+                    ],
+                }
+            )
+        )
+        resp.raise_for_status()
 
         logger.debug(
             "sent weather data to kafka, temp: %s, status: %s",
